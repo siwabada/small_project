620118bdb832ad629f11f32ced5b05c6e3d41188
siwabada <siwabada@gmail.com>
2017-02-04 23:30:12 +0900

    게시판 공지사항 정렬방식 변경

--- opentutorials/index.php
+++ opentutorials/index.php
@@ -1,11 +1,13 @@
 <?php
   require('config/config.php');
   require('lib/db.php');
-  $conn = db_init($config['host'],$config['duser'],$config['dpw'],$config['dname']);
   // DB 서버($conn)에서 조회할 테이블 (topic)의 모든 정보를 가져온다. 결과값을 $result 변수에 저장
-  $result = mysqli_query($conn, 'SELECT * FROM topic order by id desc');
+  $conn = db_init($config['host'],$config['duser'],$config['dpw'],$config['dname']);
+  // select * from board order by author = '-_-' desc, author
+  // $result = mysqli_query($conn, 'SELECT * FROM topic order by id desc');
+  // id = 94를 최상단에, id 내림차순으로 정렬
+  $result = mysqli_query($conn, 'SELECT * FROM topic order by id = 94 desc, id = 102 desc, id desc');
  ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,18 +18,6 @@
   <link rel="icon" href="favicon.png">
   <link href="/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="/style.css">
-  <script type="text/javascript">
-    function del_confirm(){
-      var result = confirm('정말로 삭제 하시겠습니까?');
-      if(result === true){
-        alert('삭제완료')
-        location.replace("/process.php?mode=delete&id=<?=$_GET['id']?>"); //해당 url 페이지로 이동시킨다.
-      }else{
-        location.replace("/index.php?id=<?=$_GET['id']?>");
-      }
-    }
-
-  </script>
 </head>
 <body>
   <div class="container col-md-8 col-lg-offset-2">
@@ -57,6 +47,7 @@
           ?>
             <tr>
               <td width="5%"><?= $i?></td>
+              <!-- identifier를 통한 comment count fetch시에는 옆의 attribute를 a 링크에 포함시킴 data-disqus-identifier='<?=$row['id']?>' -->
               <td width="50%" class="table-title"><a href="/index.php?id=<?=$row['id']?>"><?=$row['title']?></a><span class="comment"><a href="/index.php?id=<?=$row['id']?>#disqus_thread"></a></span></td>
               <td width="10%"><?= $row['author']?></td>
               <td width="15%"><?= $row['created']?></td>
@@ -78,36 +69,61 @@
         </div>
       </div>
       <a href="/modify.php?id=<?=$row['id']?>" class="btn btn-warning">modify</a>
-      <a onclick="del_confirm()" class="btn btn-danger">delete</a>
+      <a id="del_btn" class="btn btn-danger">delete</a>
       <div id="disqus_thread"></div>
     <?}?>
     </div>
   </div>
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+  var del_btn = document.getElementById('del_btn');
+  del_btn.addEventListener('click', function(){
+    var result = confirm('정말로 삭제 하시겠습니까??');
+    if(result === true){
+      alert('삭제완료')
+      //해당 url 페이지로 이동시킨다.
+      location.replace("./process.php?mode=delete&id=<?=$_GET['id']?>");
+    }else{
+      location.replace("./index.php?id=<?php $_GET['id'] ?>");
+    }
+  })
+</script>
 <!-- 댓글위젯추가 -->
 <script>
-
 /**
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
 /*
 var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+this.page.url ="http://siwabada.dothome.co.kr/index.php?id=<?=$row['id']?>";
+// Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = '<?=$row['id']?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
 */
+// DON'T EDIT BELOW THIS LINE
+// n() {
+// var d = document, s = d.createElement('script');
+// s.src = '//monkey-sunshine.disqus.com/embed.js';
+// s.setAttribute('data-timestamp', +new Date());
+// (d.head || d.body).appendChild(s);
+// })();
+
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = '//monkey-sunshine.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
+
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<!-- 댓글수 -->
 <script id="dsq-count-scr" src="//monkey-sunshine.disqus.com/count.js" async></script>
+<script>
+</script>
 </body>
 </html>
